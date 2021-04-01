e9a24ad0503936bc91c47044e762978735d800c0
tuan.pham22 <tuan.pham22@student.passerellesnumeriques.org>
2021-04-01 10:22:58 +0700

    Admin Home Page Update 10:20 a.m

--- src/Views/Admin/js/view_admin.js
+++ src/Views/Admin/js/view_admin.js
@@ -1,6 +1,17 @@
 /* Demo purposes only */
 $(".hover").mouseleave(
-    function() {
+    function () {
         $(this).removeClass("hover");
     }
 );
+
+showProduct = () => {
+    document.getElementById("show-product").style.display = "block";
+    document.getElementById("show-user").style.display = "none";
+    console.log("show Product");
+}
+showUser = () => {
+    document.getElementById("show-product").style.display = "none";
+    document.getElementById("show-user").style.display = "block";
+
+}
