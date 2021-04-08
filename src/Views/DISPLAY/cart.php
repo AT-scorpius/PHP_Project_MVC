9b1244266a98ad7ec56517a7f8068931ed0d8d8c
Tran Thi Nhu Mai <mai.tran22@pn.com.vn>
2021-04-08 08:42:20 +0700

    8/4

--- src/Views/DISPLAY/cart.php
+++ src/Views/DISPLAY/cart.php
@@ -5,6 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" href="../cart.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="js/jquery-1.11.1.min.js"></script>
 </head>
 <body>
 <div id="shopping-cart">
